6a7a0f6ad300dd38f42928159bc2fbac9d52df28
shreya.thomas <shreya.thomas18@gmail.com>
2025-05-02 12:34:28 +0530

    Adds Search functionlaity

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import Shimmer from './Shimmer';
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [restaurantList, setRestaurantList] = useState([]);
+  const [filteredRestaurantList, setFilteredRestaurantList] = useState();
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -14,17 +16,38 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    setRestaurantList(
-      json.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
+    const restaurants =
+      json.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
+    setRestaurantList(restaurants);
+    setFilteredRestaurantList(restaurants)
+  };
+
+  const handleOnSearch = () => {
+    const filteredRes = restaurantList.filter((res) => {
+      return res.info.name.toLowerCase().includes(searchText.toLowerCase());
+    });
+    setFilteredRestaurantList(filteredRes);
   };
 
   return (
     <>
-      {/* <div>Search</div> */}
+      <div className="search-container">
+        <input
+          type="text"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        ></input>
+        <button onClick={handleOnSearch}>Search</button>
+      </div>
       <div className="res-container">
-        {restaurantList.length === 0 &&  <><Shimmer/></>}
-        {restaurantList?.map((restaurant) => (
+        {restaurantList.length === 0 && (
+          <>
+            <Shimmer />
+          </>
+        )}
+        {filteredRestaurantList?.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant.info} />
         ))}
       </div>
